e704b0704b4612de44660d2ad97cb364d054bce9
KnirkeBarken <sigurd.smeby@gmail.com>
2022-10-25 18:31:17 +0200

    Finsished up a working header

--- burger.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("burgerbuttoninfo");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
